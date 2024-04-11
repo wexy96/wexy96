6bed4bb562929357be627418512f0a3c6fd26b6a
wexy96 <166761375+wexy96@users.noreply.github.com>
2024-04-12 02:16:01 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,7 +2,7 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 12/05/2024 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
 <p>⊳ Every discord server can use the Viesta bot for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
