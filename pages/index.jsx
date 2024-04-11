afd151cae1d339f5f417d71e015e0eb52646d4e9
wexy96 <166761375+wexy96@users.noreply.github.com>
2024-04-12 01:24:00 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -45,7 +45,7 @@ Vote Viesta</a>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"MusicMaker" }} />
+            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Viesta" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
             <a href="https://discord.com/oauth2/authorize?client_id=836175725518258186&permissions=8&scope=bot" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
               <i className="fab fa-discord mr-2" />Invite Viesta
@@ -86,11 +86,11 @@ Vote Viesta</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <img src="https://images-ext-1.discordapp.net/external/8DP_ayX1FL2OriI5_Wz7xtpQDwlAJvWfK6Ir-NSnwZQ/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/836175725518258186/eb5da420dbe4f64d18954c6c57d156f7.webp?format=webp&width=473&height=473"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with MusicMaker, the best and most up-to-date bot?</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Viesta, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://media.discordapp.net/attachments/907378577253744640/1176892776877666325/image.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://images-ext-1.discordapp.net/external/8DP_ayX1FL2OriI5_Wz7xtpQDwlAJvWfK6Ir-NSnwZQ/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/836175725518258186/eb5da420dbe4f64d18954c6c57d156f7.webp?format=webp&width=473&height=473" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
